574d2fbe3691cdaa0fad4fe24711709233dc063a
black671 <65527897+black671@users.noreply.github.com>
2022-01-24 23:38:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+function showNum(num) {
+    const valueInput = document.getElementById('field').value
+    document.getElementById('field').value = valueInput + num
+}
+
+function deleteValue() {
+    const valueImput = document.getElementById('field').value
+    document.getElementById('field').value = ""
+}
+
+function calculate() {
+    const valueInput = document.getElementById('field').value
+    const value = document.getElementById('field')
+    document.getElementById('field').value = eval(valueInput)
+    value.style.animation = ""
+    setTimeout(function() {value.style.animation = " anim 0.5s linear"}, 1);
+}
