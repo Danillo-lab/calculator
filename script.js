006a131f84ab1609d972bb2866d12e73600bffd8
black671 <65527897+black671@users.noreply.github.com>
2022-01-26 14:24:23 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function showNum(num) {
     const valueInput = document.getElementById('field').value
     document.getElementById('field').value = valueInput + num
@@ -15,5 +14,11 @@ function calculate() {
     const value = document.getElementById('field')
     document.getElementById('field').value = eval(valueInput)
     value.style.animation = ""
-    setTimeout(function() {value.style.animation = " anim 0.5s linear"}, 1);
+    setTimeout(function() {value.style.animation = "anim 0.5s linear"}, 1);
+}
+
+function animImg() {
+    const value = document.getElementById("image")
+    value.style.animation = ""
+    setTimeout(function() {value.style.animation = "animImg 0.3s linear"}, 1);
 }
